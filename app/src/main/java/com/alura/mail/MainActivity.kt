bd8145375268f6e990f8aad10bff2d0664ed34eb
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2024-04-25 19:38:47 -0300

    added block of code to identify type of language

--- app/src/main/java/com/alura/mail/MainActivity.kt
+++ app/src/main/java/com/alura/mail/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.alura.mail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.alura.mail.ui.navigation.HomeNavHost
 import com.alura.mail.ui.theme.MAILTheme
+import com.google.mlkit.nl.languageid.LanguageIdentification
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,6 +26,24 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val navController = rememberNavController()
                     HomeNavHost(navController = navController)
+
+                    val text = "Hello, mundo buenos dias !"
+
+                    //identify probability about the language in text
+                    val languageIdentifier = LanguageIdentification.getClient()
+                    languageIdentifier.identifyPossibleLanguages(text)
+                        .addOnSuccessListener { identifiedLanguages ->
+                            for (identifiedLanguage in identifiedLanguages) {
+                                val language = identifiedLanguage.languageTag
+                                val confidence = identifiedLanguage.confidence
+                                Log.i("language", "$language $confidence")
+                            }
+                        }
+                        .addOnFailureListener {
+                            // Model couldn’t be loaded or other internal error.
+                            // ...
+                            Log.i("error", "onCreate: $it")
+                        }
                 }
             }
         }
