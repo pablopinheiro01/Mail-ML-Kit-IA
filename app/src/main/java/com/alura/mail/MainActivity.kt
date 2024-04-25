02e9216486a0df51c3130472317ba8561b3fe873
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2024-04-25 20:06:01 -0300

    Refactory coding and treatment return

--- app/src/main/java/com/alura/mail/MainActivity.kt
+++ app/src/main/java/com/alura/mail/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.alura.mail.mlkit.translatableLanguageModels
 import com.alura.mail.ui.navigation.HomeNavHost
 import com.alura.mail.ui.theme.MAILTheme
 import com.google.mlkit.nl.languageid.LanguageIdentification
+import com.google.mlkit.nl.languageid.LanguageIdentifier
 import com.google.mlkit.nl.languageid.LanguageIdentifier.UNDETERMINED_LANGUAGE_TAG
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,28 +31,45 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     HomeNavHost(navController = navController)
 
-                    val text = "Hello, mundo buenos dias !"
+                    val text = "Hello world"
 
                     //identify probability about the language in text
-                    val languageIdentifier = LanguageIdentification.getClient()
-                    languageIdentifier.identifyLanguage(text)
-                        .addOnSuccessListener { languageCode ->
-                            val languageName: String? = translatableLanguageModels[languageCode]
-                                ?: UNDETERMINED_LANGUAGE_TAG
-                            if (languageName != UNDETERMINED_LANGUAGE_TAG) {
-                                Log.i("language", "languageName: $languageName code $languageCode  ")
-                            } else {
-                                Log.i("language", "onCreate: error")
-                            }
+                    languageIdentifier(
+                        text,
+                        onSuccess = {
+                            Log.i("language", "$it")
+                        },
+                        onFailure = {
+                            Log.i("language", "Language not identified")
+
                         }
-                        .addOnFailureListener {
-                            // Model couldn’t be loaded or other internal error.
-                            // ...
-                            Log.i("error", "onCreate: $it")
-                        }
+                    )
                 }
             }
         }
+    }
+
+    private fun languageIdentifier(
+        text: String,
+        onSuccess: (String) -> Unit = {},
+        onFailure: () -> Unit = {}
+    ) {
+        val languageIdentifier = LanguageIdentification.getClient()
+        languageIdentifier.identifyLanguage(text)
+            .addOnSuccessListener { languageCode ->
+                val languageName: String = translatableLanguageModels[languageCode]
+                    ?: UNDETERMINED_LANGUAGE_TAG
+                if (languageName != UNDETERMINED_LANGUAGE_TAG) {
+                    onSuccess(languageName)
+                } else {
+                    onFailure()
+                }
+            }
+            .addOnFailureListener {
+                // Model couldn’t be loaded or other internal error.
+                // ...
+                onFailure()
+            }
     }
 }
 
